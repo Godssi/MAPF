7509b29c4c2c529da14597869ec2317ef7aa25e4
ChoiDW614 <81404753+ChoiDW614@users.noreply.github.com>
2023-02-24 20:57:31 +0900

    commit

--- MapfCPP/MapfCPP/Heuristic_func.cpp
+++ MapfCPP/MapfCPP/Heuristic_func.cpp
@@ -23,16 +23,16 @@ double heuristic_e(pairInt node, pairInt goal)
 	return sqrt(dx * dx + dy * dy);
 }
 
-vector<pairInt> get_around_index(pairInt node, double r, Map map)
+vector<pairInt> get_around_index(pairInt node, double R, Map& map)
 {
-	pair<ll, ll> map_size = { map.size() , map[0].size() };
+	pair<ll, ll> map_size = { map.size() , map.front().size()};
 	vector<pairInt> idx;
-	for (ll i = map_size.first - r; i < map_size.first + 1; i++)
+	for (ll i = map_size.first - R; i < map_size.first + 1; i++)
 	{
-		for (ll j = map_size.second - r; j < map_size.second + 1; j++)
+		for (ll j = map_size.second - R; j < map_size.second + 1; j++)
 		{
 			double dist = sqrt(pow(i - map_size.first, 2) + pow(j - map_size.second, 2));
-			if ((0 <= i && i < map_size.first) && (0 <= j && j < map_size.second) && (r >= dist))
+			if ((0 <= i && i < map_size.first) && (0 <= j && j < map_size.second) && (R >= dist))
 			{
 				idx.push_back({ i, j });
 			}
@@ -41,18 +41,29 @@ vector<pairInt> get_around_index(pairInt node, double r, Map map)
 	return idx;
 }
 
-vector<MapIdx> get_index_to_goal_sq(pairInt node, pairInt goal)
+double get_heuristic_to_goal_sq(pairInt node, pairInt goal, double R, Map& potential_map)
 {
-	vector<MapIdx> idx;
-	for (int i = 0; i < goal.first - node.first; i++)
-		idx.push_back({ { i + node.first, i + node.second }, sqrt(2) * (i + node.first) });
-	return idx;
+	int len = abs(goal.first - node.first);
+	if (!len)
+		return 1;
+
+	double h_add = R;
+	for (int i = 0; i < abs(goal.first - node.first); i++)
+	{
+		double x = i + node.first;
+		double y = i + node.second;
+		double r = sqrt(2) * i + node.first;
+		h_add += (1 - pow(min(r, R) / (R + 0.01), 3)) * potential_map[x][y];
+	}
+	double h = R;
+	h *= (h_add / len) * sqrt(R);
+	return h;
 }
 
-pair<vector<MapIdx>, vector<MapIdx>> get_index_to_goal_rect(pairInt node, pairInt goal)
+double get_heuristic_to_goal_rect(pairInt node, pairInt goal, double R, Map& potential_map)
 {
-	vector<pair<pairInt, double>> upperIdx;
-	vector<pair<pairInt, double>> lowerIdx;
+	double h_add1 = 0, h_add2 = 0;
+	ll upperCnt = 0, lowerCnt = 0;
 	if (node.first <= goal.first and node.second <= goal.second)
 	{
 		for (int i = node.first; i < goal.first + 1; i++)
@@ -61,13 +72,13 @@ pair<vector<MapIdx>, vector<MapIdx>> get_index_to_goal_rect(pairInt node, pairIn
 			{
 				if ((goal.second - node.second + 1) * (i - goal.first) >= (goal.first - node.first + 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					upperIdx.push_back({ {i, j}, r });
+					h_add1 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					upperCnt++;
 				}
 				if ((goal.second - node.second + 1) * (i - goal.first) <= (goal.first - node.first + 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					lowerIdx.push_back({ {i, j}, r });
+					h_add2 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					lowerCnt++;
 				}
 			}
 		}
@@ -80,13 +91,13 @@ pair<vector<MapIdx>, vector<MapIdx>> get_index_to_goal_rect(pairInt node, pairIn
 			{
 				if ((goal.second - node.second + 1) * (i - goal.first) >= (goal.first - node.first - 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					upperIdx.push_back({ {i, j}, r });
+					h_add1 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					upperCnt++;
 				}
 				if ((goal.second - node.second + 1) * (i - goal.first) <= (goal.first - node.first - 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					lowerIdx.push_back({ {i, j}, r });
+					h_add2 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					lowerCnt++;
 				}
 			}
 		}
@@ -99,13 +110,13 @@ pair<vector<MapIdx>, vector<MapIdx>> get_index_to_goal_rect(pairInt node, pairIn
 			{
 				if ((goal.second - node.second - 1) * (i - goal.first) >= (goal.first - node.first - 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					upperIdx.push_back({ {i, j}, r });
+					h_add1 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					upperCnt++;
 				}
 				if ((goal.second - node.second - 1) * (i - goal.first) <= (goal.first - node.first - 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					lowerIdx.push_back({ {i, j}, r });
+					h_add2 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					lowerCnt++;
 				}
 			}
 		}
@@ -118,24 +129,31 @@ pair<vector<MapIdx>, vector<MapIdx>> get_index_to_goal_rect(pairInt node, pairIn
 			{
 				if ((goal.second - node.second - 1) * (i - goal.first) >= (goal.first - node.first + 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					upperIdx.push_back({ {i, j}, r });
+					h_add1 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					upperCnt++;
 				}
 				if ((goal.second - node.second - 1) * (i - goal.first) <= (goal.first - node.first + 1) * (j - goal.second))
 				{
-					double r = sqrt(pow(i - node.first, 2) + pow(j - node.second, 2));
-					lowerIdx.push_back({ {i, j}, r });
+					h_add2 += (1 - pow(sqrt(pow(i - node.first, 2) + pow(j - node.second, 2)) / (R + 0.01), 3)) * potential_map[i][j];
+					lowerCnt++;
 				}
 			}
 		}
 	}
-	pair<vector<MapIdx>, vector<MapIdx>> idx = { upperIdx, lowerIdx };
-	return idx;
+
+	if (!upperCnt || !lowerCnt)
+		return R;
+
+	h_add1 = (h_add1 / upperCnt) * sqrt(R);
+	h_add2 = (h_add2 / lowerCnt) * sqrt(R);
+	double h = min(h_add1, h_add2);
+
+	return h;
 }
 
-double heuristic_around_obstacle(pairInt node, pairInt goal, Map map, Map potential_map)
+double heuristic_around_obstacle(pairInt node, pairInt goal, Map& map, Map& potential_map)
 {
-	pair<ll, ll> map_size = { map.size() , map.front().size()};
+	pair<ll, ll> map_size = { map.size() , map.front().size() };
 	double r = 3;
 	double R = heuristic_e(node, goal);
 	vector<pairInt> idx = get_around_index(node, r, map);
@@ -163,42 +181,18 @@ double heuristic_around_obstacle(pairInt node, pairInt goal, Map map, Map potent
 	return h;
 }
 
-double heuristic(pairInt node, pairInt goal, Map map, Map potential_map)
+double heuristic(pairInt node, pairInt goal, Map& map, Map& potential_map)
 {
-	double h = heuristic_e(node, goal);
 	double R = heuristic_e(node, goal);
+	double h = R;
 	double beta = 40;
 
-	if (goal.first - node.first == goal.second - node.second)
-	{
-		vector<MapIdx> idx = get_index_to_goal_sq(node, goal);
-		if (idx.size() == 0)
-			return 1;
-		double h_add = 0;
-		for (int i = 0; i < idx.size(); i++)
-		{
-			h_add += (1 - pow(min(idx[i].second, R) / (R + 0.01), 3)) * potential_map[idx[i].first.first][idx[i].first.second];
-		}
-		h *= (h_add / idx.size()) * sqrt(R);
-	}
+	if (abs(goal.first - node.first) == abs(goal.second - node.second))
+		h = get_heuristic_to_goal_sq(node, goal, R, potential_map);
 	else
-	{
-		pair<vector<MapIdx>, vector<MapIdx>> idx = get_index_to_goal_rect(node, goal);
-		if (idx.first.size() == 0 || idx.second.size() == 0)
-			return 1;
-		double h_add1 = 0, h_add2 = 0;
-		for (int i = 0; i < idx.first.size(); i++)
-		{
-			h_add1 += (1 - pow(min(idx.first[i].second, R) / (R + 0.01), 3)) * potential_map[idx.first[i].first.first][idx.first[i].first.second];
-		}
-		for (int i = 0; i < idx.second.size(); i++)
-		{
-			h_add2 += (1 - pow(min(idx.second[i].second, R) / (R + 0.01), 3)) * potential_map[idx.second[i].first.first][idx.second[i].first.second];
-		}
-		h_add1 = (h_add1 / idx.first.size()) * sqrt(R);
-		h_add2 = (h_add2 / idx.second.size()) * sqrt(R);
-	}
+		h = get_heuristic_to_goal_rect(node, goal, R, potential_map);
 
 	h += heuristic_around_obstacle(node, goal, map, potential_map);
+
 	return h;
 }
