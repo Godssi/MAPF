9b89e89dc8722c1d6ce4dede8efeacdac518b0df
ChoiDW614 <81404753+ChoiDW614@users.noreply.github.com>
2023-06-01 11:23:14 +0900

    commit

--- MapfCPP/MapfCPP/main.cpp
+++ MapfCPP/MapfCPP/main.cpp
@@ -156,20 +156,12 @@ int main()
 	vector<pairInt> start = { {16,83},{35,83},{29,66},{10,66}, {3,10},{48,8},{48,34},{50,66} };
 	vector<pairInt> goal = { {40, 3},{3,63},{20,15},{48,20},{57,50},{8,50},{13,6},{35,4} };
 
-	// vector<pairInt> start = { {8, 3}, {32, 7}, {40, 22}, {34, 36}, {44, 58}, {8, 27}, {12, 42}, {13, 81} };
-	// vector<pairInt> goal = { {17, 17}, {40, 9}, {51, 22}, {46, 37}, {45, 47}, {22, 23}, {33, 37}, {14, 64} };
-
 	vector<pairInt> static_obstacle;
-	// 컨베이어 벨트 확인하는 사람의 경로 
-	//DynamicObstacle DynamicObstacle1("dy_ob1", { {4, 81}, {3, 81}, {4, 81}, {5, 81}, {6, 81}, {7, 81}, {8, 81}, {9, 81}, {10, 81}, {11, 81}, {12, 81}, {13, 81}, {14, 81}, {15, 81}, {16, 81}, {17, 81}, {18, 81}, {19, 81}, {20, 81}, {21, 81}, {22, 81}, {23, 81}, {24, 81}, {25, 81}, {26, 81}, {27, 81}, {28, 81}, {29, 81}, {30, 81}, {31, 81}, {32, 81} });
-	//// 위 물건 창고 확인하는 사람의 경로
-	//DynamicObstacle DynamicObstacle2("dy_bo2", { {2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2}, {7, 2}, {8, 2}, {9, 2}, {10, 2}, {11, 2}, {12, 2}, {13, 2}, {14, 2},{15, 2}, {16, 2}, {17, 2}, {18, 2}, {19, 2}, {20, 2}, {21, 2}, {22, 2}, {23, 2}, {23, 3}, {23, 4}, {23, 5}, {23, 6}, {23, 7}, {23, 8}, {23, 9}, {23, 10}, {23, 11}, {23, 12}, {23, 13}, {23, 14}, {23, 15}, {23, 16}, {23, 17}, {23, 18}, {23, 19}, {23, 20}, {23, 21}, {23, 22}, {23, 23} });
-	//// 아래 물건 창고 확인하는 사람의 경로
-	//DynamicObstacle DynamicObstacle3("dy_ob3", { {10, 81}, {, 81}, {51, 8}, {51, 9}, {51, 10}, {51, 11}, {51, 12}, {51, 13}, {51, 14}, {51, 15}, {51, 16}, {51, 17}, {51, 18}, {51, 19}, {51, 20}, {51, 21}, {51, 22}, {51, 23}, {51, 24}, {51, 25}, {51, 26}, {51, 27}, {51, 28}, {51, 29}, {51, 30}, {51, 31}, {51, 32}, {51, 33}, {51, 34}, {51, 35}, {51, 36}, {51, 37}, {51, 38}, {51, 39}, {51, 40}, {51, 41}, {51, 42}, {51, 43}, {51, 44}, {51, 45}, {51, 46}, {51, 47}, {52, 47}, {53, 47} });
 	
 	DynamicObstacle DynamicObstacle1("dy_ob1");
-	dynamic_path_input_x(DynamicObstacle1, 48, 19, 39, 19);
-	dynamic_path_input_y(DynamicObstacle1, 39, 20, 39, 43);
+	dynamic_path_input_y(DynamicObstacle1, 48, 19, 48, 29);
+	dynamic_path_input_x(DynamicObstacle1, 47, 29, 39, 29);
+	dynamic_path_input_y(DynamicObstacle1, 39, 30, 39, 43);
 	dynamic_path_input_x(DynamicObstacle1, 38, 43, 14, 43);
 	dynamic_path_input_y(DynamicObstacle1, 14, 42, 14, 12);
 	dynamic_path_input_x(DynamicObstacle1, 13, 12, 1, 12);
@@ -215,8 +207,9 @@ int main()
 	cout << "\n\n\n";
 
 	DynamicObstacle DynamicObstacle4("dy_ob1");
-	dynamic_path_input_x(DynamicObstacle1, 48, 19, 39, 19);
-	dynamic_path_input_y(DynamicObstacle1, 39, 20, 39, 43);
+	dynamic_path_input_y(DynamicObstacle1, 48, 19, 48, 29);
+	dynamic_path_input_x(DynamicObstacle1, 47, 29, 39, 29);
+	dynamic_path_input_y(DynamicObstacle1, 39, 30, 39, 43);
 	dynamic_path_input_x(DynamicObstacle1, 38, 43, 14, 43);
 	dynamic_path_input_y(DynamicObstacle1, 14, 42, 14, 12);
 	dynamic_path_input_x(DynamicObstacle1, 13, 12, 1, 12);
@@ -257,19 +250,19 @@ int main()
 	averageDistance2Obstacle(result);
 	print_path_text(result);
 
-	/*while (DynamicObstacle1.DoB_path.size() ||  DynamicObstacle2.DoB_path.size() || DynamicObstacle3.DoB_path.size()) {
-	   startClock = clock();
-	   vec2PInt result = planner.plan(200, 2000);
 
-	   endClock = clock();
-	   cout << "\n\n\ttime: " << endClock - startClock << "  (ms)\n";
+	//while (DynamicObstacle1.DoB_path.size() ||  DynamicObstacle2.DoB_path.size() || DynamicObstacle3.DoB_path.size()) {
+	//   startClock = clock();
+	//   vec2PInt result = planner.plan(200, 2000);
 
-	   print_path(result);
-	   if (!(planner.checkGoal(result)))
-		  break;
-	   planner.set_starts(result);
-	}*/
+	//   endClock = clock();
+	//   cout << "\n\n\ttime: " << endClock - startClock << "  (ms)\n";
 
+	//   print_path(result);
+	//   if (!(planner.checkGoal(result)))
+	//	  break;
+	//   planner.set_starts(result);
+	//}
 
 
 	return 0;
